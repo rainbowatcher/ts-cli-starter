7fb4a7e8e67cecb980d4ad69782560ab8d631fd8
rainbowatcher <rainbow-w@qq.com>
2023-07-01 11:50:22 +0800

    feat: add sample code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+import cac from "cac"
+
 export function sum(n1: number, n2: number) {
   return n1 + n2
 }
+
+function main() {
+  const cli = cac("ts-cli-starter")
+  cli.command("<first> <second>", "Sum tow number")
+    .action((first, second) => {
+      console.log(sum(Number(first), Number(second)))
+    })
+  cli.parse()
+}
+
+main()
