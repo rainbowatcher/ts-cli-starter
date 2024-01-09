42b87ac103a77ab93669a34bea9c9e36688c9b16
rainbowatcher <rainbow-w@qq.com>
2024-01-09 14:30:21 +0800

    style: change tab size to 4 whitespaces

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import cac from "cac"
 import { sum } from "./util"
 
 function main() {
-  const cli = cac("ts-cli-starter")
-  cli.command("<first> <second>", "Sum tow number")
-    .action((first, second) => {
-      console.log(sum(Number(first), Number(second)))
-    })
-  cli.parse()
+    const cli = cac("ts-cli-starter")
+    cli.command("<first> <second>", "Sum tow number")
+        .action((first, second) => {
+            console.log(sum(Number(first), Number(second)))
+        })
+    cli.parse()
 }
 
 main()
--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,3 @@
 export function sum(n1: number, n2: number) {
-  return n1 + n2
+    return n1 + n2
 }
